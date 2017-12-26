ec683bb3d65fdd61be7674c061d5b87227860a62
KwesiD <kfd233@nyu.edu>
2017-12-26 17:01:35 -0500

    editing old app

--- app.js
+++ app.js
@@ -1,18 +1,48 @@
-const express = require('express');
-const path = require("path");
-const bodyParser = require('body-parser');
-const app = express();
-const publicPath = path.resolve(__dirname, "public");
+const Alexa = require('alexa-sdk');
+const fs = require('fs');
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(publicPath));
+const APP_ID = fs.readFileSync('appId') + "";
 
+const greetingStrings = {
+ "en-US": {
+    "translation": {
+   	"GREETINGS": ["Hello","Good Morning","Good Day"]
+    }
 
+ }
 
+};
 
-app.get('/',(req,res) => {
-	res.sendFile('test.json', { root: path.join(__dirname, 'public') });
-});
+const handlers = {
+    'LaunchRequest': function () {
+        this.emit(':tell',"Let me greet you!");
+        this.emit('GetGreeting');
+    },
+    'GetGreetingIntent': function () {
+        this.emit('GetGreeting');
+    },
+    'GetGreeting': function () {
+        // Get a random space fact from the space facts list
+        // Use this.t() to get corresponding language data
+        const greetArr = this.t('GREETINGS');
+        const index = Math.floor(Math.random() * greetArr.length);
+        const randomGreeting = greetArr[index];
 
+        // Create speech output
+        //const speechOutput = this.t('GET_FACT_MESSAGE') + randomFact;
+        this.emit(':tell',randomGreeting);
+    }
+};
 
-app.listen(process.env.PORT || 3000);
+exports.handler = function (event, context) {
+   /* if(event.session.attributes){
+        event.session.attributes['foo'] = 'bar';
+    }*/
+    const alexa = Alexa.handler(event, context);
+   // console.log(event.session.attributes);
+    alexa.appId = APP_ID;
+    // To enable string internationalization (i18n) features, set a resources object.
+    alexa.resources = greetingStrings;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
